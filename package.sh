4bfc4a658e4079a5b38abb6b57b72a34fd0ed10d
Dave Hylands <dhylands@gmail.com>
2018-02-06 11:51:45 -0800

    Updated package.sh to include dependencies

--- package.sh
+++ package.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 rm -f SHA256SUMS
-sha256sum *.json *.js LICENSE > SHA256SUMS
-npm pack
+sha256sum package.json *.js LICENSE > SHA256SUMS
+TARFILE=$(npm pack)
+tar xf ${TARFILE}
+cp -r node_modules ./package
+tar cf ${TARFILE} package
+rm -rf package
+echo "Created ${TARFILE}"
