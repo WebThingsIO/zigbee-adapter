9110589abc5b7eb31427b18b2133a9c5301803ef
Dave Hylands <dhylands@gmail.com>
2018-02-06 13:00:52 -0800

    Add node_modules to files list
    and fixup package.sh to include the dependencies in SHA256SUMS

--- package.sh
+++ package.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+rm -rf node_modules
+yarn install --production
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
+find node_modules/ -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xf ${TARFILE}
 cp -r node_modules ./package
