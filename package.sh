06b81d642a3c13c2da74489c3c2a86cb81745a03
Mike Stegeman <mrstegeman@gmail.com>
2018-02-09 08:14:15 -0700

    Package add-on properly as a tgz.

--- package.sh
+++ package.sh
@@ -4,10 +4,10 @@ rm -rf node_modules
 yarn install --production
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
-find node_modules/ -type f -exec sha256sum {} \; >> SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
-tar xf ${TARFILE}
+tar xzf ${TARFILE}
 cp -r node_modules ./package
-tar cf ${TARFILE} package
+tar czf ${TARFILE} package
 rm -rf package
 echo "Created ${TARFILE}"
