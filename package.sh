04d6d957c932af289d216e1274631fb069941282
Dave Hylands <dhylands@gmail.com>
2018-04-02 10:59:05 -0700

    Updated package.sh to be compatible with addon-builder (#21)

--- package.sh
+++ package.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
 rm -rf node_modules
-yarn install --production
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  NODE_VERSION="$(node --version)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-${NODE_VERSION/\.*/}"
+fi
+if [ "${ADDON_ARCH}" == "linux-arm" ]; then
+  # We assume that CC and CXX are pointing to the cross compilers
+  yarn --ignore-scripts --production
+  npm rebuild --arch=armv6l --target_arch=arm
+else
+  yarn install --production
+fi
+
 rm -f SHA256SUMS
 sha256sum package.json *.js LICENSE > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
-TARFILE=$(npm pack)
+TARFILE="$(npm pack)"
 tar xzf ${TARFILE}
+rm ${TARFILE}
+TARFILE_ARCH="${TARFILE/.tgz/${TARFILE_SUFFIX}.tgz}"
 cp -r node_modules ./package
-tar czf ${TARFILE} package
+tar czf ${TARFILE_ARCH} package
 rm -rf package
-echo "Created ${TARFILE}"
+echo "Created ${TARFILE_ARCH}"
