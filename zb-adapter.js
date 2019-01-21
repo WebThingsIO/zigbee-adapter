2002e7c70f8945909395c5af69c9737f0239f089
Dave Hylands <dhylands@gmail.com>
2019-01-21 15:15:51 -0800

    Update zcl id packet (#90)
    * Updated zcl-id and zcl-packet to not crash with IKEA button
    
    Rebased zcl-id and zcl-packet to moziot specific versions
    which know how to parse the ZCL packets from the IKEA
    remote control.
    
    Also made sure that all of the versions were in sync so
    that zcl-id, zcl-pazcket, and zigbee-zdo all have empty
    node_modules directories.

--- zb-adapter.js
+++ zb-adapter.js
@@ -181,7 +181,7 @@ class ZigbeeAdapter extends Adapter {
     }
     const node = this.nodes[frame.nwkAddr64];
     if (!node) {
-      console.log('handleNetworkAddressResponse: Skipping:', node.addr64,
+      console.log('handleNetworkAddressResponse: Skipping:', node.nwkAddr64,
                   'due to unknown addr64');
       return;
     }
